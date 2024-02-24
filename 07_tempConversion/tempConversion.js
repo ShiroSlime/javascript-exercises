7f827a8021ce0f3c5e1995a1e0233fe9f1511cb2
ShiroSlime <158597353+ShiroSlime@users.noreply.github.com>
2024-02-24 21:39:03 +0530

    solved first 7 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+   let newTemp = (temp - 32) * (5 / 9);
+   newTemp = +newTemp.toFixed(1);
+   return newTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+   let newTemp = (temp * 9 / 5) + 32;
+   newTemp = +newTemp.toFixed(1);
+   return newTemp;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+   convertToCelsius,
+   convertToFahrenheit,
 };
